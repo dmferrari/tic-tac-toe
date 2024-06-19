82fa408eece592f5cf253268cbe9aa442f406b85
Martin Ferrari <dmferrari@gmail.com>
2024-06-19 09:15:43 -0300

    Add colors definition

---
+++ lib/colors.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Colors
+  RESET = "\e[0m"
+
+  BLUE = "\e[34m"
+  CYAN = "\e[36m"
+  DARK_GRAY = "\e[90m"
+  GREEN = "\e[32m"
+  MAGENTA = "\e[35m"
+  RED = "\e[31m"
+  WHITE = "\e[37m"
+  YELLOW = "\e[33m"
+end
